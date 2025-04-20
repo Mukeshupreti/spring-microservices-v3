abd4bdd2c79f3695eadd24dc39f3f4844d5651b2
mukeshupreti <learnjavasimply@gmail.com>
2025-04-20 17:59:13 +0200

    update

---
+++ 91.docker/hello-world-java/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HellowordRestfulWebServicesApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowordRestfulWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HellowordRestfulWebServicesApplication.class, args);
+	}
+}
