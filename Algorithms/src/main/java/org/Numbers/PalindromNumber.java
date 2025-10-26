3825f430aa28dbd65dad2f2472fc8dc5390e60cb
mu00662661 <mu00662661@techmahindra.com>
2025-10-26 18:36:59 +0100

    updated comments

--- Algorithms/src/main/java/org/Numbers/PalindromNumber.java
+++ Algorithms/src/main/java/org/Numbers/PalindromNumber.java
@@ -2,6 +2,31 @@ package org.Numbers;
 
 import java.util.Scanner;
 // url: https://leetcode.com/problems/palindrome-number/
+
+/*Given an integer x, return true if x is a palindrome, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+        Example 2:
+
+Input: x = -121 (negative)
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10 (zero in the end)
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+Constraints:
+
+        -231 <= x <= 231 - 1*/
 public class PalindromNumber {
 
     public static void main(String[] args) {
--- Algorithms/src/main/java/org/Numbers/SingleNumber.java
+++ Algorithms/src/main/java/org/Numbers/SingleNumber.java
@@ -5,6 +5,32 @@ import java.util.Scanner;
 import java.util.Set;
 
 // problem https://leetcode.com/problems/single-number/description/
+
+/*Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+*/
+
 public class SingleNumber {
 
     public static void main(String[] args) {
@@ -16,8 +42,8 @@ public class SingleNumber {
         for(int i=0;i<n;i++){
             number[i]=scanner.nextInt();
         }
-        withOneSpaceComplexcity(number);
-        withNspaceComplexicty(number);
+        withOneSpaceComplexcity(number); // using XOR self cancellation
+        withNspaceComplexicty(number);  // Hashset contains remove else add element whatever remaining is unique
 
 
 
